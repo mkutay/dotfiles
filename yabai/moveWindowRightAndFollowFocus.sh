540b96914e0138d4ab3ab9209bde47ade3d31bc8
Mehmet Kutay Bozkurt <mkutaybzkrt01101011@gmail.com>
2023-05-10 19:16:42 +0300

    Delete yabai directory

--- yabai/moveWindowRightAndFollowFocus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/dash
-curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
-
-$(yabai -m window --space next || yabai -m window --space first)
-$(yabai -m window --focus "$curWindowId")
